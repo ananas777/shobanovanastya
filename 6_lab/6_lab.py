8f16856f91ca640f9b5914154f6e685385764d8e
ananas777 <60488304+ananas777@users.noreply.github.com>
2023-10-31 19:33:49 +0300

    добавила лабораторные

---
+++ 6_lab/6_lab.py
@@ -0,0 +1,17 @@
+import numpy
+import scipy.stats
+import matplotlib.pyplot
+
+n_bins = 20
+
+dist = numpy.random.normal(50, 1, 100)
+mu, std = scipy.stats.norm.fit(dist)  
+
+matplotlib.pyplot.hist(dist, bins=25, density=True, alpha=0.6, color='b')
+
+xmin, xmax = matplotlib.pyplot.xlim() 
+x = numpy.linspace(xmin, xmax, 100) 
+p = scipy.stats.norm.pdf(x, mu, std)
+
+matplotlib.pyplot.plot(x, p, 'k', linewidth=1.5)
+matplotlib.pyplot.show()
